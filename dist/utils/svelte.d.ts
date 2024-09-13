e8c137af43004f1154c4ebb92793a81f89ddbb0a
Anthony Holland <aholland@users.noreply.github.com>
2024-09-13 18:23:58 +1200

    Add dist dir to repo

---
+++ dist/utils/svelte.d.ts
@@ -0,0 +1,3 @@
+import type { ComponentType } from 'svelte';
+export declare function useForwardEvents<T extends ComponentType | Element>(getRef: () => T): void;
+//# sourceMappingURL=svelte.d.ts.map
